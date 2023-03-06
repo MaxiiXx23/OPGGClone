571275ed2cc4b66c5d207cc2c425b9343bae9d8a
MaxiiXx23 <max.232017@gmail.com>
2023-03-06 00:31:17 -0300

    feat: menubar de idiomas adicionado.

---
+++ src/components/Header/components/ContentIdioms/styles.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components'
+
+import * as Menubar from '@radix-ui/react-menubar'
+
+export const Content = styled(Menubar.Content)`
+  overflow-y: auto;
+  max-height: 200px;
+  min-width: 220px;
+  background-color: white;
+  border-radius: 6px;
+  padding: 5px;
+  box-shadow: 0px 10px 38px -10px rgba(22, 23, 24, 0.35),
+    0px 10px 20px -15px rgba(22, 23, 24, 0.2);
+  animation-duration: 400ms;
+  animation-timing-function: cubic-bezier(0.16, 1, 0.3, 1);
+  will-change: transform, opacity;
+`
---
+++ src/database/optionsIdiom.ts
@@ -0,0 +1,123 @@
+interface OptionIdiom {
+  id: string
+  option: string
+  idiom: string
+}
+
+export const optionsIdiom: OptionIdiom[] = [
+  {
+    id: '3b2dcd0d-e9ff-4a5b-84d9-724742e8c6a0',
+    option: 'English',
+    idiom: 'English',
+  },
+  {
+    id: '50588ace-910e-40db-b85c-031aa9fc0c63',
+    option: '한국어',
+    idiom: 'Korean',
+  },
+  {
+    id: 'eb3188de-d69d-49fc-8d0f-4b04fa027398',
+    option: '日本語',
+    idiom: 'Japanese',
+  },
+  {
+    id: 'f3f75fb7-dc18-4e87-993f-d1f98d467d9c',
+    option: 'język polski',
+    idiom: 'Polish language',
+  },
+  {
+    id: '6dd7bf15-e9f9-4a1f-bc35-9a1feeedd43c',
+    option: 'français',
+    idiom: 'French',
+  },
+  {
+    id: '9d9225af-bb7b-432a-92c9-f85a004e96ed',
+    option: 'Deutsch',
+    idiom: 'German',
+  },
+  {
+    id: 'd1bc67c3-82b6-464e-8f82-d7edd5434893',
+    option: 'español',
+    idiom: 'Spanish',
+  },
+  {
+    id: '138aed77-6687-455e-97ab-39ec4e540e76',
+    option: 'Nederlands',
+    idiom: 'Nederlands',
+  },
+  {
+    id: 'fd708b01-8394-47e8-a79f-262d0c469f9d',
+    option: 'dansk',
+    idiom: 'danish',
+  },
+  {
+    id: 'bae93bf3-c99f-4feb-bd7c-9bb5feeb8e6c',
+    option: 'Svenska',
+    idiom: 'Swedish',
+  },
+  {
+    id: 'b0d713d1-023a-4d26-9898-b16a25c6f427',
+    option: 'Norsk',
+    idiom: 'Norwegian',
+  },
+  {
+    id: 'd1267efe-8ef5-4433-9218-9f066f7dc8b3',
+    option: 'русский язык',
+    idiom: 'Russian language',
+  },
+  {
+    id: '807afaaa-c6ce-41c6-ae79-a4ef90ca48ee',
+    option: 'magyar',
+    idiom: 'Hungarian',
+  },
+  {
+    id: '66773ea3-d108-4d90-917f-2c7844cdf86f',
+    option: 'suomi',
+    idiom: 'Finland',
+  },
+  {
+    id: '0674137f-6890-429a-902c-969a970030bb',
+    option: 'Türkçe',
+    idiom: 'Turkish',
+  },
+  {
+    id: '5d7466aa-aea8-425c-9ecf-bac423b75260',
+    option: 'limba română',
+    idiom: 'Romanian language',
+  },
+  {
+    id: 'd9a638f5-8b27-428c-b6f2-22661a0751e0',
+    option: 'português',
+    idiom: 'Portuguese',
+  },
+  {
+    id: '5c1f603d-592e-49fd-b900-e1a532defed2',
+    option: '简体中文',
+    idiom: 'Simplified Chinese',
+  },
+  {
+    id: '7ed4e803-ca8f-49ab-9930-f276bdfea003',
+    option: '繁體中文',
+    idiom: 'Traditional Chinese',
+  },
+  {
+    id: 'c28f3169-1404-4ac9-835c-6eb34cc63313',
+    option: 'српски језик',
+    idiom: 'Serbian language',
+  },
+  {
+    id: 'e0b19fe9-a1be-42d2-949d-446adfc31da4',
+    option: 'italiano',
+    idiom: 'Italian',
+  },
+  {
+    id: '47139868-14cf-48f2-a45c-39e4570f16b9',
+    option: 'ไทย',
+    idiom: 'Thai',
+  },
+  {
+    id: '30d8353f-4301-47ae-88d5-ed781713c60a',
+    option: 'Tiếng Việt',
+    idiom: 'Vietnamese',
+  },
+]
